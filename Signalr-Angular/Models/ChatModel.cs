76488d90cd219842eedafd83c38a0b470eddc122
unknown <dorlandode@S-R9WXLEW.ldschurch.org>
2013-06-15 00:11:50 -0600

    Initial Commit

---
+++ Signalr-Angular/Models/ChatModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Signalr_Angular.Models
+{
+    public class ChatModel
+    {
+        public delegate void ChatMessageEventHandler(string message);
+        public static ChatMessageEventHandler OnRaiseChatMessage;
+
+        public static void StartChatCommunications()
+        {
+            if(OnRaiseChatMessage != null)
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    OnRaiseChatMessage(Guid.NewGuid().ToString());
+                }
+            }
+        }
+    }
+}
---
+++ Signalr-Angular/SignalR/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.SignalR;
+using Signalr_Angular.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Signalr_Angular.SignalR
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string message)
+        {
+            ChatModel.OnRaiseChatMessage += RaiseChatMessageHandler;
+            ChatModel.StartChatCommunications();
+        }
+
+        public void RaiseChatMessageHandler(string message)
+        {
+            Clients.Caller.Receive(message);
+        }
+    }
+}
